d4288ed9982dff35cf837979226e13e558fae196
Felipe Massari <flp.massari@gmail.com>
2019-05-08 15:08:58 -0300

    depois de restaurar sistema, tudo OK

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ TRIG_left = 10
 ECHO_left = 9
 
 TRIG_right = 4 
-ECHO_right = 22
+ECHO_right = 18
 
 #Motor A, Left Side GPIO CONSTANTS
 motorR = Motor(19, 26)
@@ -32,13 +32,38 @@ GPIO.setup(TRIG_right, GPIO.OUT)
 GPIO.setup(ECHO_right, GPIO.IN)
 
 print " SETUP OK "
+setup = False
 
 #ler bluetooth autonomous ou remote
 autonomous = True
 
 #Funcoes de leitura de distancia
 
-#ler direita da direita
+#ler distancia a frente
+def front():
+	GPIO.output(TRIG_front, False)
+	time.sleep(0.5)
+
+	GPIO.output(TRIG_front, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG_front, False)
+
+	while GPIO.input(ECHO_front) == 0:
+		pulse_start = time.time()
+
+	while GPIO.input(ECHO_front) == 1:
+		pulse_end = time.time()
+
+	pulse_duration = pulse_end - pulse_start
+
+	distance_front = pulse_duration * 17150
+	distance_front = round(distance_front,2)
+	
+	if setup == False:
+		print("Sensor front, OK")
+	return(distance_front)
+
+#ler distancia da direita
 def avoid_left():
 	GPIO.output(TRIG_left, False)
 	time.sleep(0.5)
@@ -58,12 +83,13 @@ def avoid_left():
 
 	distance_left = pulse_duration_left * 17150
 	distance_left = round(distance_left,2)
-	
+		
+	if setup == False:
+		print("Sensor left, OK")
 	return(distance_left)
 
 #ler distancia da esquerda
 def avoid_right():  
-	print"toanocu"
 	GPIO.output(TRIG_right, False)
 	time.sleep(0.5)
     
@@ -74,7 +100,6 @@ def avoid_right():
 
 	while GPIO.input(ECHO_right) == 0:
 		pulse_start = time.time()
-		print"sifude" 
 		
 	while GPIO.input(ECHO_right) == 1:
 		pulse_end = time.time()
@@ -84,39 +109,37 @@ def avoid_right():
 	distance_right = pulse_duration_right * 17150
 	distance_right = round(distance_right,2)
 	
-	return(distance_right)
+	if setup == False:
+		print("Sensor right, OK")
 	
+	return(distance_right)
+
+#teste de sensores
+print("Testando sensores")
+front()
+avoid_left()
+avoid_right()
+print("Sensores OK")
+
 #Mainprogram
 while autonomous:
+	
+	setup = True
+	
+	#Modo de operacao - Autonomo ou Manual
 	modo = True #camera_control.modo()
 	
 	if modo == "manual":
 		autonomous = False
 		
 	
-	GPIO.output(TRIG_front, False)
-	time.sleep(0.5)
-
-	GPIO.output(TRIG_front, True)
-	time.sleep(0.00001)
-	GPIO.output(TRIG_front, False)
-
-	while GPIO.input(ECHO_front) == 0:
-		pulse_start = time.time()
-
-	while GPIO.input(ECHO_front) == 1:
-		pulse_end = time.time()
-
-	pulse_duration = pulse_end - pulse_start
-
-	distance_front = pulse_duration * 17150
-	distance_front = round(distance_front,2)
+	distancia = front()
 	
-	print " Distance: ", distance_front, " cm"
-	motorR.forward(0.9)
+	print " Distance: ", distancia, " cm"
+	motorR.forward(0.6)
 	motorL.forward(0.6)
 		
-	if distance_front < 12:
+	if distancia < 12:
 		
 		motorR.stop()
 		motorL.stop()
@@ -126,15 +149,15 @@ while autonomous:
 		distance_left = avoid_left()
 		distance_right = avoid_right()
 		
-		print " Distance Front: ", distance_front, " cm"
+		print " Distance Front: ", distancia, " cm"
 		print " Distance Left: ", distance_left, " cm"
 		print " Distance Right: ", distance_right, " cm"
 		
 		if distance_left < distance_right:
             #vire para a direita
 			print" turning right"
-			motorR.forward(0.7)
-			motorL.backward(1)
+			motorR.forward(0.6)
+			motorL.backward(0.6)
 			time.sleep(0.5)
 			
 			motorR.stop()
@@ -144,15 +167,14 @@ while autonomous:
 		else:
             #vire para a direita
 			print "turning left"
-			motorR.backward(0.7)
-			motorL.forward(1)
+			motorR.backward(0.6)
+			motorL.forward(0.6)
 			time.sleep(0.5)
 			
 			motorR.stop()
 			motorL.stop()
 
 
-    #ler comando autonomous ou remote
 
 	
 
